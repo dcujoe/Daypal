8e076f49e3cb00f3d7e2650a5f3eaf50fa74bf12
Daniel Agbenyega <50689568+dcujoe@users.noreply.github.com>
2021-01-08 17:51:47 +0100

    updated version of code- final

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 //Pages
+
+//End of Pages
+
+
+//Pages
 document.querySelectorAll('.logo').forEach(logo => {
-    logo.addEventListener('click',() =>{
-        document.querySelector('.front-page').style.display ='block'
-        document.querySelector('.login-page').style.display ='none'
-        document.querySelector('.signup-page').style.display ='none'
+    logo.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'block'
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'none'
     })
 })
 
 
 document.querySelectorAll('.login').forEach(loginBtn => {
-    loginBtn.addEventListener('click',() =>{
-        document.querySelector('.front-page').style.display ='none'
-        document.querySelector('.login-page').style.display ="block"
-        document.querySelector('.signup-page').style.display ="none"
+    loginBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'none'
+        document.querySelector('.login-page').style.display = "block"
+        document.querySelector('.signup-page').style.display = "none"
     })
 })
 
 
 document.querySelectorAll('.signup').forEach(signupBtn => {
-    signupBtn.addEventListener('click',() =>{
-        document.querySelector('.front-page').style.display ='none'
-        document.querySelector('.login-page').style.display ='none'
-        document.querySelector('.signup-page').style.display ='flex'
+        signupBtn.addEventListener('click', () => {
+            document.querySelector('.front-page').style.display = 'none'
+            document.querySelector('.login-page').style.display = 'none'
+            document.querySelector('.signup-page').style.display = 'flex'
+        })
     })
-})
-//End of Pages
+    //End of Pages
 
 
 
@@ -34,19 +39,54 @@ document.querySelectorAll('.signup').forEach(signupBtn => {
 //Navigation
 const dropdownItems = document.querySelectorAll('.dropdown-hover')
 
-dropdownItems.forEach(dropdownItem => {
-    dropdownItem.addEventListener('mouseover',() => {
-        dropdownItem.lastElementChild.style.cssText =
-        'opacity:1;visibility:visible'
-        document.querySelector('.navbar-wrapper').style.
-        background = 'linear-gradient(to right, #066399, #2f8fdf,#066399)'
+if (window.innerWidth < 1000) {
+    const menuIcon = document.querySelector('.menu')
+    const navbar = document.querySelector('.navbar')
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('change')
+
+        if (!navbar.classList.contains('change')) {
+            document.querySelectorAll('.nav-dropdown').forEach(dropdown => {
+                dropdown.style.left = '-20rem'
+            })
+        }
+    })
+    document.querySelectorAll('.show-dropdown').forEach(link => {
+        link.addEventListener('click', () => {
+            link.nextElementSibling.style.left = '0'
         })
-        
-        dropdownItem.addEventListener('mouseout',() => {
+    })
+    document.querySelectorAll('dropdown-heading-link').forEach(headingLink => {
+        headingLink.addEventListener('click', () => {
+            headingLink.parentElement.parentElement.style.left = '-20rem'
+        })
+    })
+} else {
+    dropdownItems.forEach(dropdownItem => {
+        dropdownItem.addEventListener('mouseover', () => {
             dropdownItem.lastElementChild.style.cssText =
-            'opacity:0;visibility:hidden'
+                'opacity:1;visibility:visible'
+            document.querySelector('.navbar-wrapper').style.
+            background = 'linear-gradient(to right, #066399, #2f8fdf,#066399)'
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(180deg)'
+        })
+
+        dropdownItem.addEventListener('mouseout', () => {
+            dropdownItem.lastElementChild.style.cssText =
+                'opacity:0;visibility:hidden'
             document.querySelector('.navbar-wrapper').style.
             background = 'none'
-            })
-})
-//End of Navigation
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(0deg)'
+        })
+    })
+    window.addEventListener('resize', () => {
+            window.location.reload()
+
+        }
+    }
+
+
+
+
+    //End of Navigation
